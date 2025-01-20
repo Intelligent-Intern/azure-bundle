0546cae24f1dba2de062aa82002221dd6d9dd6f9
Jochen Schultz <jschultz@php.net>
2025-01-20 03:35:37 +0100

    Update namespaces and docblocks for v1.0.0

--- src/AzureBundle.php
+++ src/AzureBundle.php
@@ -2,6 +2,7 @@
 
 namespace IntelligentIntern\AzureBundle;
 
+use App\DependencyInjection\Compiler\AIStrategyCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
@@ -11,5 +12,12 @@ class AzureBundle extends AbstractBundle
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import(__DIR__ . '/../config/services.yaml');
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new AIStrategyCompilerPass());
     }
 }
