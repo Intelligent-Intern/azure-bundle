eb5f40cd88f1992ea6764842f174bd01fbdfa115
Jochen Schultz <jschultz@php.net>
2025-01-21 08:43:08 +0100

    Update namespaces and docblocks for v1.0.0

--- src/Service/ChatCompletion/AzureChatCompletionService.php
+++ src/Service/ChatCompletion/AzureChatCompletionService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Service\ChatCompletion;
+namespace IntelligentIntern\AzureBundle\Service\ChatCompletion;
 
 use App\Contract\ChatHistoryInterface;
 use App\Contract\ChatCompletionServiceInterface;
--- src/Service/Embedding/AzureEmbeddingService.php
+++ src/Service/Embedding/AzureEmbeddingService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Service\Embedding;
+namespace  IntelligentIntern\AzureBundle\Service\Embedding;
 
 use App\Contract\EmbeddingServiceInterface;
 use App\Contract\LogServiceInterface;
