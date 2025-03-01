08e856628967cbf9a148760adb744a55f56ff039
Jochen Schultz <jschultz@php.net>
2025-03-02 00:13:02 +0100

    Update namespaces and docblocks for v1.0.0

--- src/AzureBundle.php
+++ src/AzureBundle.php
@@ -2,8 +2,8 @@
 
 namespace IntelligentIntern\AzureBundle;
 
-use App\DependencyInjection\Compiler\EmbeddingStrategyCompilerPass;
-use App\DependencyInjection\Compiler\ChatCompletionStrategyCompilerPass;
+use App\DependencyInjection\ChatCompletion\ChatCompletionStrategyCompilerPass;
+use App\DependencyInjection\Embedding\EmbeddingStrategyCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
